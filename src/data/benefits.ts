1ae5e05ea12129309b0ca554831c2f83d509d317
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-03-27 16:50:30 -0300

    personalizate page

---
+++ src/data/benefits.ts
@@ -0,0 +1,9 @@
+export default[
+    'Entrega Grátis',
+    'Cashback turbo',
+    'Entrega expressa',
+    'Pré-venda exclusiva',
+    'Anúncios grátis',
+    'Dobro de pontos',
+    'Cupons de desconto'
+];
--- src/pages/personalizar.tsx
+++ src/pages/personalizar.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import Head from 'next/head';
+import Buttons from '../components/Buttons';
+import Header from '../components/Header';
+import BenefitsArray from '../data/benefits';
+import styles from '../styles/pages/Personalizar.module.css';
+
 
 const Personalizar: React.FC = () => {
-  return <div />;
+  return (
+    <div className={styles.checksContainer}>
+      <Head>
+        <title>Infinity - Personalizável</title>
+      </Head>
+      <Header />
+      <h3>Personalize seu plano</h3>
+      <div>
+
+        {BenefitsArray.map((item) => (
+          <section>
+            <>
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">{item}</label>
+            </>
+          </section>
+        ))}
+      </div>
+
+      <Buttons nextScreen="/como_pagar" back="/" textButton="Continuar" />
+    </div>
+
+  );
 }
 
 export default Personalizar;
