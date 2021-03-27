5a0185f2f227843e992db5f5518ab1e8e6e9d19a
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-03-27 11:56:08 -0300

    Initial

---
+++ src/pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <meta charSet="utf-8" />
+          <link
+            rel="shortcut icon"
+            href="icons/infinite.svg"
+            type="image/svg"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
