b851cf295674071e6b134f0aa64fc21e23ea7655
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-03-27 17:07:35 -0300

    How to pay

--- src/pages/como_pagar.tsx
+++ src/pages/como_pagar.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/pages/ComoPagar.module.css';
+import CollapseCard from '../components/CollapseCard';
+import Buttons from '../components/Buttons';
+import Header from '../components/Header'; 
 
 const ComoPagar: React.FC = () => {
-  return <div />;
+  return (
+    <section className={styles.paymentContainer}>
+      <Head>
+        <title>Infinity - Como Pagar?</title>
+      </Head>
+      <Header />
+      <h3>Como deseja pagar?</h3>
+      <CollapseCard idCollapse={1} collapseTitle="Mensal" collapseContent={['R$ 40,00','Por mês']} />
+      <CollapseCard idCollapse={2} collapseTitle="Trimestral" collapseContent={['R$ 40,00','Por mês']} />
+      <CollapseCard idCollapse={3} collapseTitle="Anual" collapseContent={['R$ 40,00','Por mês']} />
+      <Buttons back="/personalizar" nextScreen="/resumo" textButton="Assinar" />
+    </section>
+  );
 }
 
 export default ComoPagar;
