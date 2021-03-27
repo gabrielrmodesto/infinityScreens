12cd339bb7ed7c28a49a46f70ea351c7fc28080c
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-03-27 12:08:57 -0300

    icons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+          <link rel="shortcut icon" href="icons/logo.png" type="image/x-icon"/>
         </Head>
         <body>
           <Main />
